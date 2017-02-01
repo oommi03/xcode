33412a4933d71f5d592eb202f5705ab5ca8baa8d
Teerawat kumsila <teerawatkumsila@Teerawats-Mac.local>
2017-02-01 02:52:38 -0800

    test

--- Calculategithub/ViewController.swift
+++ Calculategithub/ViewController.swift
@@ -22,5 +22,50 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var PlusB: UIButton!
+    
+    @IBOutlet weak var MinusB: UIButton!
+    
+    @IBOutlet weak var MultiB: UIButton!
+    
+    @IBOutlet weak var DivB: UIButton!
+    
+    
+    @IBOutlet weak var ResultL: UILabel!
+    
+    @IBOutlet weak var Value1: UITextField!
+    
+    @IBOutlet weak var Value2: UITextField!
+    
+    func getValue1 () -> Float32{
+        if (Value1.text == "") {return 0 }
+        else{
+            return Float32(Value1.text!)!
+        }
+    }
+    func getValue2 () -> Float32{
+        if (Value2.text == "") {return 0 }
+        else{
+            return Float32(Value2.text!)!
+        }
+    }
+    
+    @IBAction func PlusBPress(_ sender: UIButton) {
+        ResultL.text = String(getValue1() + getValue2())
+    }
+    @IBAction func MinusBPress(_ sender: UIButton) {
+        ResultL.text = String(getValue1() - getValue2())
+    }
+    @IBAction func MultBPress(_ sender: UIButton) {
+        ResultL.text = String(getValue1() * getValue2())
+    }
+    @IBAction func DivBPress(_ sender: UIButton) {
+        if(getValue2() == 0){
+            ResultL.text = "Nope"
+        }
+        else{
+            ResultL.text = String(getValue1() / getValue2())
+        }
+    }
 }
 
